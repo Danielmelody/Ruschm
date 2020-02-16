0ba43b848a8465aff0f81de09789879fc9b50802
Danielhu <yimingdz@gmail.com>
2020-02-16 19:42:26 +0800

    feat(lexer): full support of indentifier

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::fmt;
 use std::str;
 
@@ -36,6 +37,12 @@ struct Lexer {
     tokens: Vec<Token>,
 }
 
+macro_rules! invalid_token {
+    ($($arg:tt)*) => (
+        return Err(InvalidToken { error: format!($($arg)*) })
+    )
+}
+
 impl Lexer {
     pub fn new() -> Lexer {
         Self {
@@ -47,7 +54,7 @@ impl Lexer {
     pub fn tokenize(&mut self, input: &str) -> Result<(), InvalidToken> {
         let mut chars = input.chars();
         self.current = chars.next();
-        while let Some(c) = self.current {
+        while let Some(_) = self.current {
             self.token(&mut chars)?;
         }
         Ok(())
@@ -73,11 +80,7 @@ impl Lexer {
                                     Some(cnn) => {
                                         self.push_advance(current_iter, Token::Character(cnn))
                                     }
-                                    None => {
-                                        return Err(InvalidToken {
-                                            error: String::from("expect character after #\\"),
-                                        })
-                                    }
+                                    None => invalid_token!("expect character after #\\"),
                                 }
                             }
                             'u' => {
@@ -86,18 +89,10 @@ impl Lexer {
                                 {
                                     self.push_advance(current_iter, Token::ByteVecConsIntro);
                                 } else {
-                                    return Err(InvalidToken {
-                                        error: String::from(
-                                            "Imcomplete bytevector constant introducer",
-                                        ),
-                                    });
+                                    invalid_token!("Imcomplete bytevector constant introducer");
                                 }
                             }
-                            _ => {
-                                return Err(InvalidToken {
-                                    error: String::from("expect '(' 't' or 'f' after #"),
-                                })
-                            }
+                            _ => invalid_token!("expect '(' 't' or 'f' after #"),
                         },
                         None => (),
                     }
@@ -114,23 +109,15 @@ impl Lexer {
                     },
                     None => (),
                 },
-                '.' => match current_iter.clone().next() {
-                    Some('.') => self.identifier(current_iter)?,
-                    _ => self.push_advance(current_iter, Token::Period),
+                '.' => self.percular_identifier(current_iter)?,
+                '+' | '-' => match current_iter.clone().next() {
+                    Some('0'...'9') => self.number(current_iter)?,
+                    _ => self.percular_identifier(current_iter)?,
                 },
-                '+' | '-' => {
-                    match current_iter.clone().next() {
-                        Some('0'...'9') => self.number(current_iter)?,
-                        Some(nc) => {
-                            self.test_delimiter(nc)?;
-                            self.push_advance(current_iter, Token::Identifier(c.to_string()));
-                        }
-                        None => self.push_advance(current_iter, Token::Identifier(c.to_string())),
-                    };
-                }
                 '"' => self.string(current_iter)?,
                 '0'...'9' => self.number(current_iter)?,
-                _ => self.identifier(current_iter)?,
+                '|' => self.quote_identifier(current_iter)?,
+                _ => self.normal_identifier(current_iter)?,
             },
             None => (),
         }
@@ -159,49 +146,134 @@ impl Lexer {
         Ok(())
     }
 
-    fn identifier(&mut self, current_iter: &mut std::str::Chars) -> Result<(), InvalidToken> {
-        fn initial(current: char) -> bool {
-            match current {
-                'a'...'z'
-                | 'A'...'Z'
-                | '!'
-                | '$'
-                | '%'
-                | '&'
-                | '*'
-                | '/'
-                | ':'
-                | '<'
-                | '='
-                | '>'
-                | '?'
-                | '@'
-                | '^'
-                | '_'
-                | '~' => true,
-                _ => false,
-            }
+    fn is_identifier_initial(c: char) -> bool {
+        match c {
+            'a'...'z'
+            | 'A'...'Z'
+            | '!'
+            | '$'
+            | '%'
+            | '&'
+            | '*'
+            | '/'
+            | ':'
+            | '<'
+            | '='
+            | '>'
+            | '?'
+            | '@'
+            | '^'
+            | '_'
+            | '~' => true,
+            _ => false,
         }
+    }
+
+    fn normal_identifier(
+        &mut self,
+        current_iter: &mut std::str::Chars,
+    ) -> Result<(), InvalidToken> {
         if let Some(c) = self.current {
-            if initial(c) {
-                let mut identifier_str = String::new();
-                identifier_str.push(c);
-                loop {
-                    self.current = current_iter.next();
-                    if let Some(nc) = self.current {
-                        match nc {
-                            _ if initial(nc) => identifier_str.push(nc),
-                            '0'...'9' | '+' | '-' | '.' | '@' => identifier_str.push(nc),
-                            _ => {
-                                self.test_delimiter(nc)?;
-                                break;
-                            }
+            let mut identifier_str = String::new();
+            identifier_str.push(c);
+            loop {
+                self.current = current_iter.next();
+                if let Some(nc) = self.current {
+                    match nc {
+                        _ if Self::is_identifier_initial(nc) => identifier_str.push(nc),
+                        '0'...'9' | '+' | '-' | '.' | '@' => identifier_str.push(nc),
+                        _ => {
+                            Lexer::test_delimiter(nc)?;
+                            break;
                         }
-                    } else {
-                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            self.tokens.push(Token::Identifier(identifier_str));
+        }
+        Ok(())
+    }
+
+    fn dot_subsequent(
+        &mut self,
+        identifier_str: &mut String,
+        current_iter: &mut std::str::Chars,
+    ) -> Result<(), InvalidToken> {
+        self.current = current_iter.next();
+        if let Some(c) = self.current {
+            let valid = match c {
+                '+' | '-' | '.' | '@' => true,
+                _ => Lexer::is_identifier_initial(c),
+            };
+            match valid {
+                true => {
+                    identifier_str.push(c);
+                    loop {
+                        self.current = current_iter.next();
+                        match self.current {
+                            Some(nc) => match nc {
+                                _ if Lexer::is_identifier_initial(nc) => identifier_str.push(nc),
+                                '0'...'9' | '+' | '-' | '.' | '@' => identifier_str.push(nc),
+                                _ => {
+                                    Lexer::test_delimiter(nc)?;
+                                    break;
+                                }
+                            },
+                            None => invalid_token!("Invalid Indentifer {}", identifier_str),
+                        }
                     }
                 }
-                self.tokens.push(Token::Identifier(identifier_str));
+                false => Lexer::test_delimiter(c)?,
+            }
+        }
+        Ok(())
+    }
+
+    fn percular_identifier(
+        &mut self,
+        current_iter: &mut std::str::Chars,
+    ) -> Result<(), InvalidToken> {
+        if let Some(c) = self.current {
+            let mut identifier_str = String::new();
+            identifier_str.push(c);
+            match c {
+                '+' | '-' => {
+                    let nc = current_iter.clone().next();
+                    match nc {
+                        Some('.') => {
+                            self.current = current_iter.next();
+                            identifier_str.push('.');
+                            self.dot_subsequent(&mut identifier_str, current_iter)?;
+                        }
+                        // a dot subsequent without a dot is a sign subsequent
+                        Some(_) => self.dot_subsequent(&mut identifier_str, current_iter)?,
+                        None => self.current = current_iter.next(),
+                    }
+                }
+                '.' => self.dot_subsequent(&mut identifier_str, current_iter)?,
+                _ => (),
+            }
+            match identifier_str.as_str() {
+                "." => self.tokens.push(Token::Period),
+                _ => self.tokens.push(Token::Identifier(identifier_str)),
+            }
+        }
+        Ok(())
+    }
+
+    fn quote_identifier(&mut self, current_iter: &mut std::str::Chars) -> Result<(), InvalidToken> {
+        let mut identifier_str = String::new();
+        loop {
+            self.current = current_iter.next();
+            match self.current {
+                None => invalid_token!("Incomplete identifier {}", identifier_str),
+                Some('|') => {
+                    self.push_advance(current_iter, Token::Identifier(identifier_str));
+                    break;
+                }
+                Some(nc) => identifier_str.push(nc),
             }
         }
         Ok(())
@@ -233,26 +305,16 @@ impl Lexer {
                                         '|' => string_literal.push('|'),
                                         'x' => (), // TODO: 'x' for hex value
                                         ' ' => (), // TODO: space for nothing
-                                        _ => {
-                                            return Err(InvalidToken {
-                                                error: String::from("Unknown escape character"),
-                                            })
-                                        }
+                                        _ => invalid_token!("Unknown escape character"),
                                     }
                                 }
-                                None => {
-                                    return Err(InvalidToken {
-                                        error: String::from("Incomplete escape character"),
-                                    })
-                                }
+                                None => invalid_token!("Incomplete escape character"),
                             }
                         }
                         _ => string_literal.push(c),
                     }
                 } else {
-                    return Err(InvalidToken {
-                        error: String::from("Unclosed string literal"),
-                    });
+                    invalid_token!("Unclosed string literal")
                 }
             }
         }
@@ -272,7 +334,7 @@ impl Lexer {
                             number_str.push(nc);
                         }
                         _ => {
-                            self.test_delimiter(nc)?;
+                            Lexer::test_delimiter(nc)?;
                             break;
                         }
                     },
@@ -281,11 +343,7 @@ impl Lexer {
             }
             match number_str.parse() {
                 Ok(number) => self.tokens.push(Token::Number(number)),
-                Err(_) => {
-                    return Err(InvalidToken {
-                        error: format!("Unrecognized number: {}", number_str),
-                    })
-                }
+                Err(_) => invalid_token!("Unrecognized number: {}", number_str),
             }
         }
         Ok(())
@@ -296,12 +354,10 @@ impl Lexer {
         self.current = current_iter.next();
     }
 
-    fn test_delimiter(&mut self, c: char) -> Result<(), InvalidToken> {
+    fn test_delimiter(c: char) -> Result<(), InvalidToken> {
         match c {
             ' ' | '\t' | '\n' | '\r' | '(' | ')' | '"' | ';' | '|' => Ok(()),
-            _ => Err(InvalidToken {
-                error: format!("Expect delimiter here instead of {}", c),
-            }),
+            _ => invalid_token!("Expect delimiter here instead of {}", c),
         }
     }
 }
@@ -340,13 +396,30 @@ fn simple_tokens() -> Result<(), InvalidToken> {
 #[test]
 fn identifier() -> Result<(), InvalidToken> {
     let mut l = Lexer::new();
-    l.tokenize("+ - xyz123_")?;
+    l.tokenize(
+        "
+    ... +
+    +soup+ <=?
+    ->string a34kTMNs
+    lambda list->vector
+    q V17a
+    |two words| |two; words|",
+    )?;
     assert_eq!(
         l.tokens,
         vec![
+            Token::Identifier(String::from("...")),
             Token::Identifier(String::from("+")),
-            Token::Identifier(String::from("-")),
-            Token::Identifier(String::from("xyz123_"))
+            Token::Identifier(String::from("+soup+")),
+            Token::Identifier(String::from("<=?")),
+            Token::Identifier(String::from("->string")),
+            Token::Identifier(String::from("a34kTMNs")),
+            Token::Identifier(String::from("lambda")),
+            Token::Identifier(String::from("list->vector")),
+            Token::Identifier(String::from("q")),
+            Token::Identifier(String::from("V17a")),
+            Token::Identifier(String::from("two words")),
+            Token::Identifier(String::from("two; words"))
         ]
     );
 
@@ -386,14 +459,14 @@ fn string() -> Result<(), InvalidToken> {
 #[test]
 fn number() -> Result<(), InvalidToken> {
     let mut l = Lexer::new();
-    l.tokenize("+123 -123 + +123")?;
+    l.tokenize("+123 -123 + -123")?;
     assert_eq!(
         l.tokens,
         vec![
             Token::Number(123),
             Token::Number(-123),
             Token::Identifier(String::from("+")),
-            Token::Number(123),
+            Token::Number(-123),
         ]
     );
     Ok(())
