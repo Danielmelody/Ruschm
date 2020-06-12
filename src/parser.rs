1c45c0dfffc00abde290a5c80e8247ca46b1ac43
Danielhu <yimingdz@gmail.com>
2020-06-12 14:40:22 +0800

    basic types

---
+++ src/parser.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code)]
+use std::fmt;
+use std::iter::Iterator;
+
+struct Ast {
+    Expression,
+    LeftParen,
+    RightParen,
+    Number,
+    ProceduralCall,
+    Operator,
+    Operand
+}
+
+
