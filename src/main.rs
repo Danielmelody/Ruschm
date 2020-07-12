b0486e85fab92373cb4038c0b7b5cce6596948b1
Danielhu <yimingdz@gmail.com>
2020-07-12 21:00:21 +0800

    refactor: move buildin primitives to scheme/base

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,14 @@
-mod env;
 mod error;
 mod interpreter;
 mod lexer;
 mod parser;
-use std::io;
-use std::io::{BufRead, Write};
+mod repl;
 
-fn eval<'a>(
-    it: &'a interpreter::Interpreter<'a>,
-    char_stream: impl Iterator<Item = char>,
-) -> Result<Option<interpreter::ValueType>, error::Error> {
-    let ast: Result<parser::ParseResult, _> = lexer::TokenGenerator::new(char_stream).collect();
-    // let parser = parser::Parser::new(tokens?.into_iter());
-    it.eval_root_ast(&ast??)
-}
+use std::env;
 
 fn main() {
-    let stdin = io::stdin();
-    let it = interpreter::Interpreter::new();
-    print!("==> ");
-    io::stdout().flush().unwrap();
-    // let mut source = String::new();
-    for line in stdin.lock().lines() {
-        match eval(&it, line.unwrap().chars()) {
-            Ok(opt) => {
-                if let Some(value) = opt {
-                    println!("{}", value);
-                }
-            }
-            Err(e) => eprintln!("{}", e),
-        }
-        io::stdout().flush().unwrap();
-        print!("==> ");
-        io::stdout().flush().unwrap();
+    match env::args().skip(1).next() {
+        Some(_) => (), // TODO
+        None => repl::run(),
     }
 }
---
+++ src/repl.rs
@@ -0,0 +1,71 @@
+use crate::interpreter;
+use std::io;
+use std::io::Write;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+fn check_bracket_closed(chars: impl Iterator<Item = char>) -> bool {
+    let mut count = 0;
+    let mut in_comment = false;
+    for c in chars {
+        match (c, in_comment) {
+            ('(', false) => count = count + 1,
+            (')', false) => count = count - 1,
+            (';', false) => in_comment = true,
+            ('\n', true) => in_comment = false,
+            _ => (),
+        }
+    }
+    count <= 0
+}
+
+pub fn run() {
+    let it = interpreter::Interpreter::new();
+    let mut rl = Editor::<()>::new();
+    io::stdout().flush().unwrap();
+    let mut source = String::new();
+
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    println!("Ruschm Version {}", VERSION);
+    loop {
+        let readline = match source.is_empty() {
+            true => rl.readline("> "),
+            false => rl.readline(""),
+        };
+        match readline {
+            Ok(line) => {
+                if line.is_empty() {
+                    continue;
+                }
+                source.push_str(line.as_str());
+                source.push_str("\n");
+                if check_bracket_closed(source.chars()) {
+                    match it.eval(source.chars()) {
+                        Ok(opt) => {
+                            if let Some(value) = opt {
+                                println!("{}", value);
+                            }
+                        }
+                        Err(e) => eprintln!("{}", e),
+                    }
+                    source.clear();
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                source.clear();
+                println!("Interrupted input by ctrl-c, use ctrl-d to exit.");
+                continue;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("exited. have a nice day.");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+}
