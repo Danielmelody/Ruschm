5b35951f98306040aab407398bf7ce07cba4dc5c
yiminghu <yiminghu@tencent.com>
2020-08-07 16:44:29 +0800

    chore: add  fibonacci sequence example

---
+++ examples/fib.scm
@@ -0,0 +1,14 @@
+(define (fib x)
+    (if (< x 2)
+        1
+        (+ (fib (- x 2)) (fib (- x 1)))))
+
+(define fib-seq
+    (lambda x
+        (if (< x 1) 1
+            (fib-seq (- x 1)))
+        (display (fib x))
+        (newline)
+        1))
+
+(fib-seq 26)
