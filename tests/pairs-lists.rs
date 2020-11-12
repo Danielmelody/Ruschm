fbd77ddeab1145afc18a8ac7df488ded38426ba8
Danielhu <yimingdz@gmail.com>
2020-11-12 10:05:44 +0800

    ci: code coverage actions

--- tests/pairs-lists.rs
+++
@@ -1,9 +0,0 @@
-use ruschm::{environment::StandardEnv, error::SchemeError, interpreter::*};
-
-#[test]
-fn pairs() -> Result<(), SchemeError> {
-    let source = include_str!("pairs.scm");
-    let it = Interpreter::<f32, StandardEnv<_>>::new();
-    it.eval(source.chars())?;
-    Ok(())
-}
--- tests/pairs.scm
+++
@@ -1,9 +0,0 @@
-(define a (cons 1 2))
-(define b '(1 . 2))
-
-'(
-(pair? a)
-(pair? b)
-(car a)
-(cdr b)
-)
