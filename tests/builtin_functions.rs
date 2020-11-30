5baa4bb7e735fd34c0e2f2ec990e7dd014af148a
Daniel <yimingdz@gmail.com>
2020-11-30 15:06:13 +0800

    fix: panic inside apply
    close #71

--- tests/builtin_functions.rs
+++ tests/builtin_functions.rs
@@ -1,8 +1,10 @@
 use ruschm::{
     environment::StandardEnv,
-    error::SchemeError,
-    interpreter::{pair::Pair, Interpreter},
+    error::ToLocated,
+    error::{ErrorData, SchemeError},
+    interpreter::{error::LogicError, pair::Pair, Interpreter},
     values::Number,
+    values::Type,
     values::Value,
 };
 
@@ -21,6 +23,20 @@ fn list() -> Result<(), SchemeError> {
                 )))
             )))
         ))))
+    );
+    Ok(())
+}
+
+#[test]
+fn apply() -> Result<(), SchemeError> {
+    let mut interpreter = Interpreter::<f32, StandardEnv<f32>>::new_with_stdlib();
+    assert_eq!(
+        interpreter.eval("(apply + 1 2 '(3 4))".chars())?,
+        Some(Value::Number(Number::Integer(10)))
+    );
+    assert_eq!(
+        interpreter.eval("(apply + 1)".chars()),
+        Err(ErrorData::Logic(LogicError::TypeMisMatch("1".to_owned(), Type::Pair)).no_locate())
     );
     Ok(())
 }
